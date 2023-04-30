e2778a647afc6f8e8084bc7f7e555747879f7c4a
Dahiya, Amit <Amit.Dahiya@adidas.com>
2023-04-30 23:09:14 +0530

    added code for spring jdbc

--- spring.jdbc/src/main/java/com/simplilearn/spring/jdbc/App.java
+++ spring.jdbc/src/main/java/com/simplilearn/spring/jdbc/App.java
@@ -1,20 +1,27 @@
 package com.simplilearn.spring.jdbc;
 
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 /**
  * Hello world!
- *
  */
 public class App {
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		EmployeeDao employeeDao = (EmployeeDao) applicationContext.getBean("employeedao");
-		
-		int result = employeeDao.saveEmployee(new Employee(100, "John", 100000));
-		
-		System.out.println(result);
-	}
+  public static void main (String[] args) {
+
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+        "applicationContext.xml");
+
+    EmployeeDao employeeDao = (EmployeeDao) applicationContext.getBean("employeedao");
+
+//		int result = employeeDao.saveEmployee(new Employee(100, "John", 100000));
+
+//		int result = employeeDao.updateEmployee(new Employee(100, "Sam", 50000));
+
+    int result = employeeDao.deleteEmployee(new Employee(100));
+    System.out.println(result);
+  }
 }
